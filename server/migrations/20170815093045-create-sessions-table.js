82cb0e3f153e18c8fefe8843613392b32214e298
Maarten Mulders <m.th.mulders@xs4all.nl>
2017-08-17 08:25:28 +0200

    Type of user session data is JSON

--- server/migrations/20170815093045-create-sessions-table.js
+++ server/migrations/20170815093045-create-sessions-table.js
@@ -4,7 +4,7 @@ var type = dbm.dataType;
 exports.up = function(db, callback) {
   db.createTable('sessions', {
     sid   : { type: 'string'   , notNull: true, primaryKey: true },
-    sess  : { type: 'string'   , notNull: true },
+    sess  : { type: 'json'     , notNull: true },
     expire: { type: 'timestamp', notNull: true },
   }, callback);
 };
