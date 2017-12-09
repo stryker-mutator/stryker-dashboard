1454a65321b334c46bbdf79a06e4d78245b278e5
Peter Toonen <Peter.Toonen@infosupport.com>
2017-12-09 10:51:11 +0100

    comma's to semicolumns

--- packages/badge-service/badgeGenerator/badgeGeneratorSpec.ts
+++ packages/badge-service/badgeGenerator/badgeGeneratorSpec.ts
@@ -62,132 +62,132 @@ describe('Generating a badge', () => {
 
         // Assert
         expect(context.res.status).to.equal(400);
-    }),
-        it('Should return a http 400 error when the combination of slug & branch can not be found', async () => {
-            // Arrange
-            context.bindingData.branch = 'dev';
+    });
+    it('Should return a http 400 error when the combination of slug & branch can not be found', async () => {
+        // Arrange
+        context.bindingData.branch = 'dev';
 
-            // Act
-            await run(context, req);
+        // Act
+        await run(context, req);
 
-            // Assert
-            expect(context.res.status).to.equal(400);
-        }),
-        it('Should return a http 500 error when storage can not be reached', async () => {
-            // Arrange
-            mutationScoreMapperMock.selectSingleEntity.reset();
-            mutationScoreMapperMock.selectSingleEntity.throws("error");
+        // Assert
+        expect(context.res.status).to.equal(400);
+    });
+    it('Should return a http 500 error when storage can not be reached', async () => {
+        // Arrange
+        mutationScoreMapperMock.selectSingleEntity.reset();
+        mutationScoreMapperMock.selectSingleEntity.throws("error");
 
-            // Act
-            await run(context, req);
+        // Act
+        await run(context, req);
 
-            // Assert
-            expect(context.res.status).to.equal(500);
-        }),
-        it('Should return a http 500 error when shields.io returns an empty result', async () => {
-            // Arrange
-            sandbox.stub(httpHelpers, 'getContent').resolves(undefined);
+        // Assert
+        expect(context.res.status).to.equal(500);
+    });
+    it('Should return a http 500 error when shields.io returns an empty result', async () => {
+        // Arrange
+        sandbox.stub(httpHelpers, 'getContent').resolves(undefined);
 
-            // Act
-            await run(context, req);
+        // Act
+        await run(context, req);
 
-            // Assert
-            expect(context.res.status).to.equal(500);
-        }),
-        it('Should return a http 500 error when shields.io can not be reached', async () => {
-            // Arrange
-            const content = 'this really is an image string';
-            sandbox.stub(httpHelpers, 'getContent').throws('cannot reach it');
+        // Assert
+        expect(context.res.status).to.equal(500);
+    });
+    it('Should return a http 500 error when shields.io can not be reached', async () => {
+        // Arrange
+        const content = 'this really is an image string';
+        sandbox.stub(httpHelpers, 'getContent').throws('cannot reach it');
 
-            // Act
-            await run(context, req);
+        // Act
+        await run(context, req);
 
-            // Assert
-            expect(context.res.status).to.equal(500);
-        }),
-        it('Should call shields.io with the correct parameters without branch', async () => {
-            // Arrange
-            sandbox.stub(httpHelpers, 'getContent').resolves('your image');
-            delete context.bindingData.branch;
+        // Assert
+        expect(context.res.status).to.equal(500);
+    });
+    it('Should call shields.io with the correct parameters without branch', async () => {
+        // Arrange
+        sandbox.stub(httpHelpers, 'getContent').resolves('your image');
+        delete context.bindingData.branch;
 
-            // Act
-            await run(context, req);
+        // Act
+        await run(context, req);
 
-            // Assert
-            expect(context.res.status).to.equal(200);
-            expect(httpHelpers.getContent).calledWith('https://img.shields.io/badge/mutation%20score-79-orange.svg');
-        }),
-        it('Should call shields.io with the correct parameters', async () => {
-            // Arrange
-            sandbox.stub(httpHelpers, 'getContent').resolves('your image');
+        // Assert
+        expect(context.res.status).to.equal(200);
+        expect(httpHelpers.getContent).calledWith('https://img.shields.io/badge/mutation%20score-79-orange.svg');
+    });
+    it('Should call shields.io with the correct parameters', async () => {
+        // Arrange
+        sandbox.stub(httpHelpers, 'getContent').resolves('your image');
 
-            // Act
-            await run(context, req);
+        // Act
+        await run(context, req);
 
-            // Assert
-            expect(context.res.status).to.equal(200);
-            expect(httpHelpers.getContent).calledWith('https://img.shields.io/badge/mutation%20score-97.8-green.svg');
-        }),
-        it('Should return correct cache headers', async () => {
-            // Arrange
-            sandbox.stub(httpHelpers, 'getContent').resolves('your image');
+        // Assert
+        expect(context.res.status).to.equal(200);
+        expect(httpHelpers.getContent).calledWith('https://img.shields.io/badge/mutation%20score-97.8-green.svg');
+    });
+    it('Should return correct cache headers', async () => {
+        // Arrange
+        sandbox.stub(httpHelpers, 'getContent').resolves('your image');
 
-            // Act
-            await run(context, req);
+        // Act
+        await run(context, req);
 
-            // Assert
-            expect(context.res.status).to.equal(200);
-            expect(context.res.headers).to.deep.equal({
-                'Cache-Control': 'no-cache, no-store, must-revalidate',
-                'Pragma': 'no-cache'
-            });
-        }),
-        it('Should return a green shield with a score >= 80', async () => {
-            // Arrange
-            sandbox.stub(httpHelpers, 'getContent').resolves('green');
-            mutationScore.score= 80;
-            
-            // Act
-            await run(context, req);
-
-            // Assert
-            expect(context.res.status).to.equal(200);
-            expect(httpHelpers.getContent).calledWith('https://img.shields.io/badge/mutation%20score-80-green.svg');
-        }),
-        it('Should return an orange shield with a 60 <= score < 80', async () => {
-            // Arrange
-            sandbox.stub(httpHelpers, 'getContent').resolves('orange');
-            mutationScore.score= 79.9;
-            
-            // Act
-            await run(context, req);
-
-            // Assert
-            expect(context.res.status).to.equal(200);
-            expect(httpHelpers.getContent).calledWith('https://img.shields.io/badge/mutation%20score-79.9-orange.svg');
-        }),
-        it('Should return an orange shield with a score = 60', async () => {
-            // Arrange
-            sandbox.stub(httpHelpers, 'getContent').resolves('orange');
-            mutationScore.score= 60;
-            
-            // Act
-            await run(context, req);
-
-            // Assert
-            expect(context.res.status).to.equal(200);
-            expect(httpHelpers.getContent).calledWith('https://img.shields.io/badge/mutation%20score-60-orange.svg');
-        }),
-        it('Should return a red shield with a score < 60', async () => {
-            // Arrange
-            sandbox.stub(httpHelpers, 'getContent').resolves('red');
-            mutationScore.score= 59.9;
-
-            // Act
-            await run(context, req);
-
-            // Assert
-            expect(context.res.status).to.equal(200);
-            expect(httpHelpers.getContent).calledWith('https://img.shields.io/badge/mutation%20score-59.9-red.svg');
+        // Assert
+        expect(context.res.status).to.equal(200);
+        expect(context.res.headers).to.deep.equal({
+            'Cache-Control': 'no-cache, no-store, must-revalidate',
+            'Pragma': 'no-cache'
         });
+    });
+    it('Should return a green shield with a score >= 80', async () => {
+        // Arrange
+        sandbox.stub(httpHelpers, 'getContent').resolves('green');
+        mutationScore.score = 80;
+
+        // Act
+        await run(context, req);
+
+        // Assert
+        expect(context.res.status).to.equal(200);
+        expect(httpHelpers.getContent).calledWith('https://img.shields.io/badge/mutation%20score-80-green.svg');
+    });
+    it('Should return an orange shield with a 60 <= score < 80', async () => {
+        // Arrange
+        sandbox.stub(httpHelpers, 'getContent').resolves('orange');
+        mutationScore.score = 79.9;
+
+        // Act
+        await run(context, req);
+
+        // Assert
+        expect(context.res.status).to.equal(200);
+        expect(httpHelpers.getContent).calledWith('https://img.shields.io/badge/mutation%20score-79.9-orange.svg');
+    });
+    it('Should return an orange shield with a score = 60', async () => {
+        // Arrange
+        sandbox.stub(httpHelpers, 'getContent').resolves('orange');
+        mutationScore.score = 60;
+
+        // Act
+        await run(context, req);
+
+        // Assert
+        expect(context.res.status).to.equal(200);
+        expect(httpHelpers.getContent).calledWith('https://img.shields.io/badge/mutation%20score-60-orange.svg');
+    });
+    it('Should return a red shield with a score < 60', async () => {
+        // Arrange
+        sandbox.stub(httpHelpers, 'getContent').resolves('red');
+        mutationScore.score = 59.9;
+
+        // Act
+        await run(context, req);
+
+        // Assert
+        expect(context.res.status).to.equal(200);
+        expect(httpHelpers.getContent).calledWith('https://img.shields.io/badge/mutation%20score-59.9-red.svg');
+    });
 });    
--- packages/badge-service/scoreProcessor/scoreProcessorSpec.ts
+++ packages/badge-service/scoreProcessor/scoreProcessorSpec.ts
@@ -59,110 +59,110 @@ describe('Posting a Score', () => {
 
     // assert
     expect(context.res.status).to.equal(400);
-  }),
-    it('should fail when no slug is provided.', async () => {
-      // Arrange
-      req.body.repositorySlug = undefined;
+  });
+  it('should fail when no slug is provided.', async () => {
+    // Arrange
+    req.body.repositorySlug = undefined;
 
-      // Act
-      await run(context, req);
+    // Act
+    await run(context, req);
 
-      // Assert
-      expect(context.res.status).to.equal(400);
-    }),
-    it('should fail when no score is provided.', async () => {
-      // Arrange
-      req.body.mutationScore = undefined;
+    // Assert
+    expect(context.res.status).to.equal(400);
+  });
+  it('should fail when no score is provided.', async () => {
+    // Arrange
+    req.body.mutationScore = undefined;
 
-      // Act
-      await run(context, req);
+    // Act
+    await run(context, req);
 
-      // Assert
-      expect(context.res.status).to.equal(400);
-    }),
-    it('should fail when no API key is provided.', async () => {
-      // Arrange
-      req.body.apiKey = undefined;
+    // Assert
+    expect(context.res.status).to.equal(400);
+  });
+  it('should fail when no API key is provided.', async () => {
+    // Arrange
+    req.body.apiKey = undefined;
 
-      // Act
-      await run(context, req);
+    // Act
+    await run(context, req);
 
-      // Assert
-      expect(context.res.status).to.equal(400);
-    }),
-    it('should save the mutation score.', async () => {
-      // arrange
-      const expectedMutationScore = new MutationScore();
-      expectedMutationScore.branch = 'master';
-      expectedMutationScore.slug = 'github/stryker-mutator/stryker';
-      expectedMutationScore.score = 97.8;
+    // Assert
+    expect(context.res.status).to.equal(400);
+  });
+  it('should save the mutation score.', async () => {
+    // arrange
+    const expectedMutationScore = new MutationScore();
+    expectedMutationScore.branch = 'master';
+    expectedMutationScore.slug = 'github/stryker-mutator/stryker';
+    expectedMutationScore.score = 97.8;
 
-      mutationScoreMapperMock.insertOrMergeEntity.resolves();
+    mutationScoreMapperMock.insertOrMergeEntity.resolves();
 
-      // act
-      await run(context, req);
+    // act
+    await run(context, req);
 
-      // assert
-      expect(mutationScoreMapperMock.insertOrMergeEntity).calledWith(expectedMutationScore);
-      expect(context.res.status).to.equal(201);
-    }),
-    it('should result in an unauthorized error if an invalid slug is provided', async () => {
-      // arrange
-      req.body.repositorySlug = 'invalidslugwithoutseparators';
+    // assert
+    expect(mutationScoreMapperMock.insertOrMergeEntity).calledWith(expectedMutationScore);
+    expect(context.res.status).to.equal(201);
+  });
+  it('should result in an unauthorized error if an invalid slug is provided', async () => {
+    // arrange
+    req.body.repositorySlug = 'invalidslugwithoutseparators';
 
-      // act
-      await run(context, req);
+    // act
+    await run(context, req);
 
-      // assert
-      expect(context.res.status).to.equal(403);
-    }),
-    it('should save score when no branch is provided', async () => {
-      // arrange
-      const expectedMutationScore = new MutationScore();
-      expectedMutationScore.slug = 'github/stryker-mutator/stryker';
-      expectedMutationScore.score = 97.8;
-      expectedMutationScore.branch = '';
+    // assert
+    expect(context.res.status).to.equal(403);
+  });
+  it('should save score when no branch is provided', async () => {
+    // arrange
+    const expectedMutationScore = new MutationScore();
+    expectedMutationScore.slug = 'github/stryker-mutator/stryker';
+    expectedMutationScore.score = 97.8;
+    expectedMutationScore.branch = '';
 
-      delete req.body.branch;
+    delete req.body.branch;
 
-      mutationScoreMapperMock.insertOrMergeEntity.resolves();
+    mutationScoreMapperMock.insertOrMergeEntity.resolves();
 
-      // act
-      await run(context, req);
+    // act
+    await run(context, req);
 
-      // assert
-      expect(mutationScoreMapperMock.insertOrMergeEntity).calledWith(expectedMutationScore);
-      expect(context.res.status).to.equal(201);
-    }),
-    it('should throw a permission denied when an invalid API key is used', async () => {
-      // Arrange
-      req.body.apiKey = 'this is an invalid API key';
+    // assert
+    expect(mutationScoreMapperMock.insertOrMergeEntity).calledWith(expectedMutationScore);
+    expect(context.res.status).to.equal(201);
+  });
+  it('should throw a permission denied when an invalid API key is used', async () => {
+    // Arrange
+    req.body.apiKey = 'this is an invalid API key';
 
-      // Act
-      await run(context, req);
+    // Act
+    await run(context, req);
 
-      // Assert
-      expect(context.res.status).to.equal(403);
-    }),
-    it('should return an http error if project table can not be accessed', async () => {
-      // Arrange
-      projectMapperStub.selectSingleEntity.reset();
-      projectMapperStub.selectSingleEntity.throws('error');
+    // Assert
+    expect(context.res.status).to.equal(403);
+  });
+  it('should return an http error if project table can not be accessed', async () => {
+    // Arrange
+    projectMapperStub.selectSingleEntity.reset();
+    projectMapperStub.selectSingleEntity.throws('error');
 
-      // Act
-      await run(context, req);
+    // Act
+    await run(context, req);
 
-      // Assert
-      expect(context.res.status).to.equal(500);
-    }),
-    it('should return an http error if score can not be saved', async () => {
-      // Arrange
-      mutationScoreMapperMock.insertOrMergeEntity.throws('error');
+    // Assert
+    expect(context.res.status).to.equal(500);
+  });
+  it('should return an http error if score can not be saved', async () => {
+    // Arrange
+    mutationScoreMapperMock.insertOrMergeEntity.throws('error');
 
-      // Act
-      await run(context, req);
+    // Act
+    await run(context, req);
 
-      // Assert
-      expect(context.res.status).to.equal(500);
-    })
+    // Assert
+    expect(context.res.status).to.equal(500);
+  });
 });
