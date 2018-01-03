7d0f4d3d171e7ba9b397028caf2050ad5fbddac0
Peter Toonen <peter@toonen.io>
2018-01-03 12:44:57 +0000

    feat(badge): Add two azure functions (#5)
    Add new package that contains 2 azure functions. One for retrieving the badge and one for posting the badge
    
    * Rewrite data access module to work with badges
    * Add function.json to run as azure functions

--- packages/data-access/src/index.ts
+++ packages/data-access/src/index.ts
@@ -2,3 +2,4 @@
 export { default as MutationScoreMapper } from './MutationScoreMapper';
 export { default as ProjectMapper } from './ProjectMapper';
 export { default as Project } from './models/Project';
+export { default as MutationScore } from './models/MutationScore';
