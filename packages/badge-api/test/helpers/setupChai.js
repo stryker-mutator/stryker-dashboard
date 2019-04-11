6998ae538d9b8c8082b86e8828dce719efc4c0da
Nico Jansen <nico.jansen@infosupport.com>
2019-04-11 19:20:00 +0200

    build: clean build files

--- packages/badge-api/test/helpers/setupChai.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const chai = require("chai");
-const sinonChai = require("sinon-chai");
-chai.use(sinonChai);
-//# sourceMappingURL=setupChai.js.map
--- packages/badge-api/test/helpers/setupSinon.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sinon = require("sinon");
-afterEach(() => {
-    sinon.restore();
-});
-//# sourceMappingURL=setupSinon.js.map
