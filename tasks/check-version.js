0e47548ddb99b484254ff891833950385cbbd52e
Nico Jansen <nico.jansen@infosupport.com>
2019-10-05 00:24:35 +0200

    Make check-version more forgiving

--- tasks/check-version.js
+++ tasks/check-version.js
@@ -18,28 +18,52 @@ function httpClient() {
 const [, , type, url, expectedVersion] = process.argv;
 console.log(`Validating ${url} for version ${expectedVersion}`);
 if (url && expectedVersion && type === 'dashboard' || type === 'badge-api') {
-  httpClient()(url, resp => {
-    if (type === 'dashboard') {
-      verifyDashboardVersion(resp);
-    } else {
-      expect(resp.headers['x-badge-api-version'], 'Validating x-badge-api-version').eq(expectedVersion);
-    }
-    console.log(`✅  ${expectedVersion} installed at ${url} ^-^`)
-  });
+  tryCheckVersion();
 } else {
   console.info(`Usage: node ${path.basename(__filename)} [badge-api/dashboard] url version`);
   console.info(`Example: node ${path.basename(__filename)} dashboard https://dashboard.stryker-mutator.io/api/version 1.0.0\``);
   process.exitCode = 1;
 }
 
-function verifyDashboardVersion(resp) {
-  let data = '';
-  resp.on('data', chunk => data += chunk);
-  resp.on('end', () => {
-    const versionInfo = JSON.parse(data);
-    expect(versionInfo).deep.eq({
-      dashboard: expectedVersion,
-      frontend: expectedVersion
+function tryCheckVersion(attemptsLeft = 5) {
+  httpClient()(url, res => {
+    checkResponse(res).catch(err => {
+      if (attemptsLeft === 0) {
+        console.error(err);
+        process.exitCode = 1;
+      } else {
+        console.log(`Failed, ${err} trying ${attemptsLeft} more time(s)`);
+        setTimeout(() => tryCheckVersion(attemptsLeft - 1), 5000);
+      }
     });
   });
+}
+
+async function checkResponse(resp) {
+  if (type === 'dashboard') {
+    await verifyDashboardVersion(resp);
+  } else {
+    expect(resp.headers['x-badge-api-version'], 'Validating x-badge-api-version').eq(expectedVersion);
+  }
+  console.log(`✅  ${expectedVersion} installed at ${url} ^-^`)
+}
+
+function verifyDashboardVersion(resp) {
+  return new Promise((res, rej) => {
+    let data = '';
+    resp.on('data', chunk => data += chunk);
+    resp.on('end', () => {
+      const versionInfo = JSON.parse(data);
+      try {
+        expect(versionInfo).deep.eq({
+          dashboard: expectedVersion,
+          frontend: expectedVersion
+        });
+        res();
+      } catch (err) {
+        rej(err);
+      }
+    });
+    resp.on('err', err => rej(err));
+  })
 }
