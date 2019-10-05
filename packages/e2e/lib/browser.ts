7fa081bf3246a4b115497a53f7fd0d84a667c268
Nico Jansen <nico.jansen@infosupport.com>
2019-10-05 17:35:51 +0200

    Configure the browser to only run in headless mode on the build server

--- packages/e2e/lib/browser.ts
+++ packages/e2e/lib/browser.ts
@@ -4,23 +4,18 @@ import { Options } from 'selenium-webdriver/chrome';
 let browser: WebDriver | null = null;
 
 export async function init() {
-
+  const options = new Options().windowSize({
+    width: 2000,
+    height: 1000
+  });
+  if (process.env.GITHUB_WORKFLOW) {
+    console.log('Detecting build server. Using headless chrome');
+    options.headless();
+  }
   browser = await new Builder()
     .forBrowser('chrome')
-    .setChromeOptions(new Options().headless().windowSize({
-      width: 640,
-      height: 480
-    }))
+    .setChromeOptions(options)
     .build();
-
-  // const headlessCapabilities = Capabilities.chrome();
-  // // if (process.env.GITHUB_WORKFLOW) {
-  // console.log('Detecting buildserver. Using headless chrome');
-  // // headlessCapabilities.set()
-  // headlessCapabilities.set('chromeOptions', { args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage'] });
-  // // }
-  // const chromeService = new ServiceBuilder().build();
-  // browser = await Driver.createSession(headlessCapabilities, chromeService);
 }
 
 export function getCurrent(): WebDriver {
