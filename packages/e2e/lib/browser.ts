ffd13e5813e04c7c91a6b11988b7d016ce3a804d
Nico Jansen <nico.jansen@infosupport.com>
2019-10-05 17:26:36 +0200

    Enforce chrome is headless

--- packages/e2e/lib/browser.ts
+++ packages/e2e/lib/browser.ts
@@ -1,17 +1,26 @@
-import { Capabilities, Builder, WebDriver } from 'selenium-webdriver';
+import { WebDriver, Builder } from 'selenium-webdriver';
+import { Options } from 'selenium-webdriver/chrome';
 
 let browser: WebDriver | null = null;
 
 export async function init() {
-  const headlessCapabilities = Capabilities.chrome();
-  if (process.env.GITHUB_WORKFLOW) {
-    console.log('Detecting buildserver. Using headless chrome');
-    headlessCapabilities.set('chromeOptions', { args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage'] });
-  }
+
   browser = await new Builder()
     .forBrowser('chrome')
-    .withCapabilities(headlessCapabilities)
+    .setChromeOptions(new Options().headless().windowSize({
+      width: 640,
+      height: 480
+    }))
     .build();
+
+  // const headlessCapabilities = Capabilities.chrome();
+  // // if (process.env.GITHUB_WORKFLOW) {
+  // console.log('Detecting buildserver. Using headless chrome');
+  // // headlessCapabilities.set()
+  // headlessCapabilities.set('chromeOptions', { args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage'] });
+  // // }
+  // const chromeService = new ServiceBuilder().build();
+  // browser = await Driver.createSession(headlessCapabilities, chromeService);
 }
 
 export function getCurrent(): WebDriver {
