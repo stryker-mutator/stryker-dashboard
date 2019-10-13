a61759eac0d86b1597fff97c442252e074e703a1
Nico Jansen <nico.jansen@infosupport.com>
2019-10-13 17:51:35 +0200

    Use npm to determine next free canary version

--- tasks/set-npm-canary-version.js
+++ tasks/set-npm-canary-version.js
@@ -1,24 +1,45 @@
-/**
- * This file contains an implementation to determine the canary version using Lerna's algorithm.
- * Unfortenetly Lerna doesn't export this functionality in a nicer way.
- */
-const describeRef = require('@lerna/describe-ref');
-const semver = require('semver');
-const core = require('@actions/core');
+// @ts-check
 
-const ref = determineRef();
-const preId = sanitize(ref);
-console.log(`PreId: ${preId}`);
-describeRef()
-  .then(({ lastVersion, refCount }) => {
-    const npmVersion = `${semver.inc(lastVersion, 'patch')}-${preId}.${Math.max(0, refCount - 1)}`;
-    core.exportVariable('NPM_PACKAGE_VERSION', npmVersion);
-    console.log(npmVersion);
-  })
-  .catch(err => {
-    console.err(err);
-    process.exitCode = 1;
-  });
+/**
+ * This file will determine the next canary version number, setting it in the NPM_PACKAGE_VERSION variable for all next actions to use.
+ * Instead of Lerna's algorithm, it validates that the next version does not yet exist.
+ * Unfortunately Lerna itself doesn't support this
+ */
+const core = require('@actions/core');
+const axios = require('axios').default;
+const semver = require('semver');
+const describeRef = require('@lerna/describe-ref');
+
+
+determineNextCanaryVersion().catch(err => {
+  console.error(err);
+  process.exitCode = 1;
+});
+
+async function determineNextCanaryVersion() {
+  const ref = determineRef();
+  const preId = sanitize(ref);
+  const nextPatchVersion = await describeNextPatchVersion();
+  const { versions } = (await axios('https://registry.npmjs.org/@stryker-mutator/dashboard-backend')).data;
+  const revision = determineNextFreeRevision(nextPatchVersion, preId, versions);
+  const npmVersion = formatVersion(nextPatchVersion, preId, revision);
+  core.exportVariable('NPM_PACKAGE_VERSION', npmVersion);
+  console.log(npmVersion);
+}
+
+/**
+ * @param {string} version 
+ * @param {string} preId 
+ * @param {number} revision
+ */
+function formatVersion(version, preId, revision) {
+    return `${version}-${preId}.${revision}`;
+  }
+
+async function describeNextPatchVersion() {
+  const { lastVersion } = await describeRef();
+  return semver.inc(lastVersion, 'patch');
+}
 
 /**
  * @param {string} ref 
@@ -30,12 +51,22 @@ function sanitize(ref) {
 
 function determineRef() {
   const rawRef = process.env.GITHUB_REF;
-  console.log(`Raw ref: ${rawRef}`);
-  // Ref will be in the form "refs/pull/:prNumber/merge" or "refs/heads/feat/branch-1"
+  if(!rawRef){
+    throw new Error('Env variable GITHUB_REF was not set!');
+  }
+  // rawRef will be in the form "refs/pull/:prNumber/merge" or "refs/heads/feat/branch-1"
   const [, type, ...name] = rawRef.split('/');
   if (type === 'pull') {
     return `pr-${name[0]}`
   } else {
     return name.join('/');
   }
+}
+
+function determineNextFreeRevision(version, preId, existingVersions) {
+  let revision = 0;
+  while (formatVersion(version, preId, revision) in existingVersions) {
+    revision++;
+  }
+  return revision;
 }
