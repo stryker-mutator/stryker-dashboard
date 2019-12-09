61acde697b326625204351102ed9491254cd1d5a
Nico Jansen <jansennico@gmail.com>
2019-12-09 08:19:21 +0100

    test(e2e): add manual stress test

---
+++ packages/e2e/testResources/manual-stress-test.js
@@ -0,0 +1,27 @@
+// @ts-check
+const axios = require('axios').default;
+const fs = require('fs').promises;
+const path = require('path');
+const httpClient = axios.create({ baseURL: 'http://localhost:4200' });
+
+async function uploadAll() {
+  const fileNames = await fs.readdir(path.resolve(__dirname, 'stryker-reports'));
+
+  const files = await Promise.all(fileNames.map(async name => ({
+    name,
+    content: JSON.parse(await fs.readFile(path.resolve(__dirname, 'stryker-reports', name), 'utf8'))
+  })));
+  await Promise.all(files.map(async file => {
+    const parts = file.name.split('_')
+    const moduleName = parts[parts.length - 1].substr(0, parts[parts.length - 1].indexOf('.'));
+    const projectAndVersion = parts.slice(0, parts.length - 1).join('/')
+    const result = await httpClient.put(`/api/reports/${projectAndVersion}?module=${moduleName}`, file.content, {
+      headers: {
+        ['X-Api-Key']: '<API_KEY>',
+      }
+    });
+    console.log(result.status, result.statusText, result.data);
+  }));
+}
+
+uploadAll().catch(err => console.error(err));
