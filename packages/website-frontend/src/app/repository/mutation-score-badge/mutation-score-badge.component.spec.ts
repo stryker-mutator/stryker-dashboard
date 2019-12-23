34f1089a356b02014df93872426efefcc5b9199d
Nico Jansen <jansennico@gmail.com>
2019-12-23 08:03:47 +0100

    feat(front-end): Add mutation-score-badge-component

---
+++ packages/website-frontend/src/app/repository/mutation-score-badge/mutation-score-badge.component.spec.ts
@@ -0,0 +1,85 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MutationScoreBadgeComponent } from './mutation-score-badge.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { createRepository } from 'src/app/testHelpers/mock.spec';
+import { badgeSrc } from '../util';
+
+describe('MutationScoreBadgeComponent', () => {
+  let component: MutationScoreBadgeComponent;
+  let fixture: ComponentFixture<MutationScoreBadgeComponent>;
+  let el: HTMLElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [MutationScoreBadgeComponent],
+      imports: [RouterTestingModule]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MutationScoreBadgeComponent);
+    component = fixture.componentInstance;
+    el = fixture.nativeElement;
+  });
+
+  it('should show the correct image', async () => {
+    // Arrange
+    const repo = createRepository({
+      slug: 'github.com/fooOrg/barName',
+      defaultBranch: 'dev',
+      name: 'barName'
+    });
+    component.repo = repo;
+
+    // Act
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const img = el.querySelector('img');
+
+    // Assert
+    expect(img).not.toBeNull();
+    expect(img.src).toBe(badgeSrc(repo, 'flat'));
+    expect(img.alt).toBe('Badge for barName on the dev branch.');
+  });
+
+  it('should show different styles based on the "style" attribute', async () => {
+    // Arrange
+    component.repo = createRepository();
+    component.style = 'for-the-badge';
+
+    // Act
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const img = el.querySelector('img');
+
+    // Assert
+    const url = new URL(img.src);
+    expect(url.searchParams.get('style')).toBe('for-the-badge');
+  });
+
+  it('should not show a link by default', async () => {
+    // Arrange
+    component.repo = createRepository();
+
+    // Act
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    // Assert
+    expect(el.querySelector('a')).toBeNull();
+  });
+
+  it('should show a link when linkEnabled is true', async () => {
+    // Arrange
+    component.repo = createRepository({ slug: 'fooOwner/fooRepo', defaultBranch: 'dev' });
+    component.enableLink = true;
+
+    // Act
+    fixture.detectChanges();
+    await fixture.whenStable();
+    const anchor = el.querySelector('a');
+
+    // Assert
+    expect(anchor).not.toBeNull();
+    expect(anchor.href.endsWith('reports/fooOwner/fooRepo/dev')).toBeTruthy('Not end with reports/fooOwner/fooRepo/dev');
+  });
+});
---
+++ packages/website-frontend/src/app/repository/mutation-score-badge/mutation-score-badge.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Repository } from '@stryker-mutator/dashboard-contract';
+import { badgeSrc, BadgeStyle } from '../util';
+
+@Component({
+  selector: 'stryker-mutation-score-badge',
+  templateUrl: './mutation-score-badge.component.html',
+  styleUrls: ['./mutation-score-badge.component.scss']
+})
+export class MutationScoreBadgeComponent {
+
+  @Input()
+  public repo!: Repository;
+
+  @Input()
+  public style: BadgeStyle = 'flat';
+
+  @Input()
+  public enableLink = false;
+
+  public get src() {
+    return badgeSrc(this.repo, this.style);
+  }
+
+  public get alt() {
+    return `Badge for ${this.repo.name} on the ${this.repo.defaultBranch} branch.`;
+  }
+}
