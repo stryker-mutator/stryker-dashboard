fe25f5aedb78f7f06333bd06aad06d24f2007e20
Nico Jansen <jansennico@gmail.com>
2019-12-23 08:04:16 +0100

    feat(front-end): add generic clipboard component

--- packages/website-frontend/src/app/shared/shared.module.ts
+++ packages/website-frontend/src/app/shared/shared.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from './loading/loading.component';
+import { ClipboardCopyComponent } from './clipboard-copy/clipboard-copy.component';
 
 @NgModule({
   declarations: [
-    LoadingComponent
+    LoadingComponent,
+    ClipboardCopyComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    LoadingComponent
+    LoadingComponent,
+    ClipboardCopyComponent
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ]
 })
 export class SharedModule { }
