1cf54259b48850a39bac8b45d5f924ce6d346053
Nico Jansen <jansennico@gmail.com>
2020-01-13 13:30:19 +0100

    Github Actions: Use local chromedriver

--- tasks/webdriver-manager-update.js
+++ tasks/webdriver-manager-update.js
@@ -1,10 +1,7 @@
 const { execSync } = require('child_process');
 // See https://help.github.com/en/github/automating-your-workflow-with-github-actions/software-in-virtual-environments-for-github-actions#ubuntu-1804-lts
-//  and https://sites.google.com/a/chromium.org/chromedriver/
-const chromeDriverVersionBuildServer = '77.0.3865.40';
-if (process.env.GITHUB_ACTION) {
-  console.info('Detecting build server, downloading', chromeDriverVersionBuildServer)
-  execSync(`npx webdriver-manager update --versions.chrome ${chromeDriverVersionBuildServer}`, { stdio: 'inherit'});
+if (process.env.ChromeWebDriver) {
+  console.info('Detecting build server, using ', process.env.ChromeWebDriver)
 } else {
   execSync('npx webdriver-manager update', { stdio: 'inherit'});
 }
