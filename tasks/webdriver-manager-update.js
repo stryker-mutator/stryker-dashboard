3e39e459bb69f703c8eb2dda99be018054dc835b
Nico Jansen <jansennico@gmail.com>
2020-11-17 20:31:22 +0100

    chore(e2e): change owner of chrome driver

--- tasks/webdriver-manager-update.js
+++ tasks/webdriver-manager-update.js
@@ -1,8 +1,8 @@
 const { execSync } = require('child_process');
 // See https://help.github.com/en/github/automating-your-workflow-with-github-actions/software-in-virtual-environments-for-github-actions#ubuntu-1804-lts
 if (process.env.CHROMEWEBDRIVER) {
-  execSync(`sudo chmod +x ${process.env.CHROMEWEBDRIVER}`, { stdio: 'inherit'})
   console.info('Detecting build server, using ', process.env.CHROMEWEBDRIVER)
+  execSync(`sudo chown $USER ${process.env.CHROMEWEBDRIVER}`, { stdio: 'inherit'})
 } else {
   execSync('npx webdriver-manager update', { stdio: 'inherit'});
 }
