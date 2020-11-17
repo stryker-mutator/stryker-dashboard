da5b3c395d0a5ed077c4299428616a65474cfbd9
Nico Jansen <jansennico@gmail.com>
2020-11-17 20:41:05 +0100

    chore(e2e): change owner and mode of chrome driver

--- tasks/webdriver-manager-update.js
+++ tasks/webdriver-manager-update.js
@@ -3,6 +3,9 @@ const { execSync } = require('child_process');
 if (process.env.CHROMEWEBDRIVER) {
   console.info('Detecting build server, using ', process.env.CHROMEWEBDRIVER)
   execSync(`sudo chown $USER ${process.env.CHROMEWEBDRIVER}`, { stdio: 'inherit'})
+  execSync(`sudo chmod 777 ${process.env.CHROMEWEBDRIVER}`, { stdio: 'inherit'})
+  console.info(`sudo chown ${process.env.USER} ${process.env.CHROMEWEBDRIVER}`)
+  console.info(`sudo chmod 777 ${process.env.CHROMEWEBDRIVER}`)
 } else {
   execSync('npx webdriver-manager update', { stdio: 'inherit'});
 }
