c95ad181d629f73c45af79dad198ce4e06f25fb3
Nico Jansen <jansennico@gmail.com>
2020-11-17 19:37:17 +0100

    chore(e2e): add retry mechanism in download task

--- tasks/download-badge-api-package.sh
+++ tasks/download-badge-api-package.sh
@@ -2,7 +2,23 @@
 # After that, the package directory can be deployed to azure with package deploy
 set -e
 NPM_PACKAGE_VERSION=$1
-wget $(npm view @stryker-mutator/dashboard-badge-api@${NPM_PACKAGE_VERSION} dist.tarball)
+
+
+n=0
+max=10
+until [ "$n" -ge $max ]
+do
+   wget $(npm view @stryker-mutator/dashboard-badge-api@${NPM_PACKAGE_VERSION} dist.tarball) && break
+   echo "Failed get npm view @stryker-mutator/dashboard-badge-api@${NPM_PACKAGE_VERSION} dist.tarball, retrying after 10 sec"
+   n=$((n+1)) 
+   sleep 10
+done
+
+if [[ $n -ge $max ]]; then
+  echo "Failed $max times, exiting" >&2
+  exit 1
+fi
+
 tar -xzf dashboard-badge-api-${NPM_PACKAGE_VERSION}.tgz
 cd package
 npm install --production
