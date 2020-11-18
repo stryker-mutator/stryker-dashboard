1efbd5bc6eddb139da133613eabbe0df96e12b20
Nico Jansen <jansennico@gmail.com>
2020-11-18 08:25:03 +0100

    chore: use preinstalled chromedriver in pipeline

--- tasks/webdriver-manager-update.js
+++ tasks/webdriver-manager-update.js
@@ -1,11 +1,8 @@
 const { execSync } = require('child_process');
+const path = require('path');
 // See https://help.github.com/en/github/automating-your-workflow-with-github-actions/software-in-virtual-environments-for-github-actions#ubuntu-1804-lts
 if (process.env.CHROMEWEBDRIVER) {
-  console.info('Detecting build server, using ', process.env.CHROMEWEBDRIVER)
-  execSync(`sudo chown $(id -u):$(id -g)  ${process.env.CHROMEWEBDRIVER}`, { stdio: 'inherit'})
-  execSync(`sudo chmod 777 ${process.env.CHROMEWEBDRIVER}`, { stdio: 'inherit'})
-  console.info(`sudo chown $(id -u):$(id -g) ${process.env.CHROMEWEBDRIVER}`)
-  console.info(`sudo chmod 777 ${process.env.CHROMEWEBDRIVER}`)
+  console.info(`[${path.basename(__filename)}] Detecting build server, using ${process.env.CHROMEWEBDRIVER}/chromedriver`)
 } else {
   execSync('npx webdriver-manager update', { stdio: 'inherit'});
 }
