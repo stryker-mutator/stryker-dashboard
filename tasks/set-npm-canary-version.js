a883e85ed2bf6d6193dce837e61b16fcc7da13eb
Nico Jansen <jansennico@gmail.com>
2021-12-31 10:10:33 +0100

    chore(dev-deps): re-enable e2e test and ci test
    Fix all errors that crept in after years of inactivity.
    
    * Disabled 2 e2e tests that no longer work (protractor doesn't seem to support shadow dom on latest chrome release).
    * Fix the canary npm package publish
    * Fixed karma config for chrome. Something with proxies..? don't know why.

--- tasks/set-npm-canary-version.js
+++ tasks/set-npm-canary-version.js
@@ -5,13 +5,12 @@
  * Instead of Lerna's algorithm, it validates that the next version does not yet exist.
  * Unfortunately Lerna itself doesn't support this
  */
-const core = require('@actions/core');
-const axios = require('axios').default;
-const semver = require('semver');
-const describeRef = require('@lerna/describe-ref');
+const core = require("@actions/core");
+const axios = require("axios").default;
+const semver = require("semver");
+const { version: currentVersion } = require("../lerna.json");
 
-
-determineNextCanaryVersion().catch(err => {
+determineNextCanaryVersion().catch((err) => {
   console.error(err);
   process.exitCode = 1;
 });
@@ -19,47 +18,44 @@ determineNextCanaryVersion().catch(err => {
 async function determineNextCanaryVersion() {
   const ref = determineRef();
   const preId = sanitize(ref);
-  const nextPatchVersion = await describeNextPatchVersion();
-  const { versions } = (await axios('https://registry.npmjs.org/@stryker-mutator/dashboard-backend')).data;
+  const nextPatchVersion = semver.inc(currentVersion, "patch");
+  const { versions } = (
+    await axios("https://registry.npmjs.org/@stryker-mutator/dashboard-backend")
+  ).data;
   const revision = determineNextFreeRevision(nextPatchVersion, preId, versions);
   const npmVersion = formatVersion(nextPatchVersion, preId, revision);
-  core.exportVariable('NPM_PACKAGE_VERSION', npmVersion);
+  core.exportVariable("NPM_PACKAGE_VERSION", npmVersion);
   console.log(npmVersion);
 }
 
 /**
- * @param {string} version 
- * @param {string} preId 
+ * @param {string} version
+ * @param {string} preId
  * @param {number} revision
  */
 function formatVersion(version, preId, revision) {
-    return `${version}-${preId}.${revision}`;
-  }
-
-async function describeNextPatchVersion() {
-  const { lastVersion } = await describeRef();
-  return semver.inc(lastVersion, 'patch');
+  return `${version}-${preId}.${revision}`;
 }
 
 /**
- * @param {string} ref 
+ * @param {string} ref
  */
 function sanitize(ref) {
   // Sanitizes a github ref name to be a valid pre-id according to semver spec: https://semver.org/#spec-item-9
-  return ref.replace(/\//g, '-');
+  return ref.replace(/\//g, "-");
 }
 
 function determineRef() {
   const rawRef = process.env.GITHUB_REF;
-  if(!rawRef){
-    throw new Error('Env variable GITHUB_REF was not set!');
+  if (!rawRef) {
+    throw new Error("Env variable GITHUB_REF was not set!");
   }
   // rawRef will be in the form "refs/pull/:prNumber/merge" or "refs/heads/feat/branch-1"
-  const [, type, ...name] = rawRef.split('/');
-  if (type === 'pull') {
-    return `pr-${name[0]}`
+  const [, type, ...name] = rawRef.split("/");
+  if (type === "pull") {
+    return `pr-${name[0]}`;
   } else {
-    return name.join('/');
+    return name.join("/");
   }
 }
 
