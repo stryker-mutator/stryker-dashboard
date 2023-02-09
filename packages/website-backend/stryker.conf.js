54f3891f786d1be7d9dccd6c854dd586b154e177
Nico Jansen <jansennico@gmail.com>
2023-02-09 15:34:56 +0100

    test(mutation): fix mutation testing (#264)

--- packages/website-backend/stryker.conf.js
+++ packages/website-backend/stryker.conf.js
@@ -1,5 +1,13 @@
-import settings from '../../stryker.parent.conf';
+import fs from 'fs';
+
+const settings = JSON.parse(
+  fs.readFileSync(
+    new URL('../../stryker.parent.conf.json', import.meta.url),
+    'utf-8'
+  )
+);
 settings.dashboard.module = import.meta.url.split('/').slice(-2)[0];
+
 /**
  * @type {import('@stryker-mutator/api/core').PartialStrykerOptions}
  */
