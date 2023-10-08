68e94c935857820ec5ae63abdc77f142aa51cfeb
Xander Vedder <xander.vedd@gmail.com>
2023-10-08 10:47:33 +0200

    fix: add `js` extension to import

--- packages/e2e/spec/real-time-report-api.spec.ts
+++ packages/e2e/spec/real-time-report-api.spec.ts
@@ -1,27 +1,33 @@
-import test, { expect } from '@playwright/test';
-import { ReportClient } from '../po/reports/report-client.po';
-import { pendingReport } from '../actions/report.action';
+import { test, expect } from '@playwright/test';
+import { ReportClient } from '../po/reports/report-client.po.js';
+import { pendingReport } from '../actions/report.action.js';
 import type { PutReportResponse } from '@stryker-mutator/dashboard-contract';
 import type { Report } from '@stryker-mutator/dashboard-common';
 import { MutantStatus } from 'mutation-testing-report-schema';
 
 test.describe('Real-time api', () => {
   let client: ReportClient;
+  let report: Report;
 
   test.beforeEach(({ request }) => {
     client = new ReportClient(request);
+    report = createPendingReport();
   });
 
   test.afterEach(async () => {
-    await client.deletePendingReport(createPendingReport());
+    await client.deletePendingReport(report);
   });
 
   test.describe('HTTP PUT', () => {
     test('should accept pending reports', async ({ baseURL }) => {
-      const response = await uploadPendingReport(createPendingReport());
+      const response = await uploadPendingReport(report);
       const expectedResponse: PutReportResponse = {
         href: new URL(
-          'github.com/stryker-mutator-test-organization/hello-org/main',
+          'reports/github.com/stryker-mutator-test-organization/hello-org/main?realTime=true',
+          baseURL
+        ).toString(),
+        projectHref: new URL(
+          'reports/github.com/stryker-mutator-test-organization/hello-org/main',
           baseURL
         ).toString(),
       };
@@ -32,13 +38,12 @@ test.describe('Real-time api', () => {
 
   test.describe('HTTP POST', () => {
     test('should accept a batch of mutants', async () => {
-      const report = createPendingReport();
       await uploadPendingReport(report);
 
       const response = await client.postMutantBatch(report, [
         { id: '1', status: MutantStatus.Killed },
       ]);
-      expect(response.ok).toBe(true);
+      expect(response.ok()).toBe(true);
     });
   });
 
