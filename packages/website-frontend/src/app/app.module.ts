b935a8edaaa5e38ff6aa296833f929bd7825008a
Jens K <contact@jensk.dev>
2023-10-13 22:22:16 +0200

    chore: prepare for component imports

--- packages/website-frontend/src/app/app.module.ts
+++ packages/website-frontend/src/app/app.module.ts
@@ -19,7 +19,8 @@ import { AppRouterModule } from './app-router.module';
 /* Import preflight styles */
 import "@stryker-mutator/ui-components/dist/style.css"
 /* Node resolution inside angular project does not support importing by subpath exports, so we directly import the component export by file */
-import "@stryker-mutator/ui-components/dist/exports/test";
+// import "@stryker-mutator/ui-components/dist/exports/profile-button";
+// import "@stryker-mutator/ui-components/dist/exports/sign-in-button";
 
 @NgModule({
   bootstrap: [AppComponent],
