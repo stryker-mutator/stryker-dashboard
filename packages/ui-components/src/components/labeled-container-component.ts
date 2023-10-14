7ab75ed508ee33bd5cf4270a4b88a8bea2fb72e9
Jair Josafath <jairjosafath@gmail.com>
2023-10-14 13:45:57 +0200

    feat(ui): commented dummy element for testing

--- packages/ui-components/src/components/labeled-container-component.ts
+++ packages/ui-components/src/components/labeled-container-component.ts
@@ -4,17 +4,16 @@ import { property } from 'lit/decorators.js';
 
 export class LabeledContainerComponent extends BaseElement {
   @property({ type: String})
-  title: string ='';
+  label: string ='';
 
   render() {
     return html`
       <div class="flex justify-center content-center p-10">
-        <div class="border-neutral-500/80 border-2 rounded hover:-translate-y-[1px]">
-          <p class="text-xl font-semibold bg-neutral-500/80 text-neutral-100 p-1">${this.title}</p>
-        <!-- <slot></slot> -->
-        <div class="h-96 w-96 text-white bg-red-600/10">Dummy content</div>
+        <div class="border-neutral-600 border-2 rounded hover:-translate-y-[1px]">
+          <p class="text-xl font-semibold bg-neutral-600 text-neutral-100 p-1 cursor-default">${this.label}</p>
+        <slot></slot>
+        <!-- <div class="h-96 w-96 text-white bg-red-600/10">Dummy content</div> -->
         </div>
-        
       </div>
     `;
   }
