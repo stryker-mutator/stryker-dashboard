62e3c7da635daef9349d5d515c74441ad2fef37e
Lex Krooswijk <lexkrooswijk@live.nl>
2023-10-14 10:08:26 +0200

    remove addition to the welcome component

--- packages/website-frontend/src/app/welcome/welcome.component.ts
+++ packages/website-frontend/src/app/welcome/welcome.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user/user.service';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
-import { TestElement } from 'ui-components';
 
 @Component({
   selector: 'stryker-welcome',
