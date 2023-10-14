f65817c943824e7c927194ef80733de3aad14221
Jair Josafath <jairjosafath@gmail.com>
2023-10-14 11:02:53 +0200

    feat(ui): removed borders in mobile view and enlarged the text

--- packages/ui-components/src/components/status-box-component.ts
+++ packages/ui-components/src/components/status-box-component.ts
@@ -14,11 +14,11 @@ export class StatusBoxComponent extends BaseElement {
   render() {
     return html` 
     <div class="flex justify-center items-center bg-neutral-800 p-6">
-    <div class="w-11/12 md:flex md:justify-around max-w-4xl grid justify-center content-around md:h-full h-96">
+    <div class="w-11/12 md:flex md:justify-around md:max-w-4xl grid justify-center content-around md:h-full h-auto py-16">
     ${this.boxes.map(({ value, label, color }) => html`
-      <div class="flex flex-col items-center flex-nowrap border-[3px] border-neutral-600 md:px-4 py-2 rounded sm:w-52 px-0 w-48 h-fit">
-        <label class="${color} lg:text-4xl md:text-2xl font-bold text-2xl">${value}</label>
-        <label class="text-gray-100 font-bold md:text-lg">${label}</label>
+      <div class="flex flex-col items-center flex-nowrap md:border-[3px]  border-neutral-600 md:px-4 md:py-2 py-8 rounded sm:w-52 px-0 w-48 h-fit">
+        <label class="${color} lg:text-4xl md:text-2xl font-bold text-6xl">${value}</label>
+        <label class="text-gray-100 font-bold md:text-lg text-2xl whitespace-nowrap">${label}</label>
       </div>
       `)
       }
