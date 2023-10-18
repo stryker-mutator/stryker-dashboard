6b2933afb18773a74568da80433ce70579fcd7c6
Jens K <contact@jensk.dev>
2023-10-18 13:21:48 +0200

    refactor: simplified gzipped tailwind bundle sizes
    simplified bundle generation by only using 2 stylesheets; one that is imported globally, and one that is reused per component

--- packages/ui-components/src/base.ts
+++ packages/ui-components/src/base.ts
@@ -1,19 +1,8 @@
 import { LitElement, unsafeCSS } from 'lit';
 
-import components from './tailwind-styles/components.css?inline';
-import utilities from './tailwind-styles/utilities.css?inline';
-import screens from './tailwind-styles/screens.css?inline';
-import globals from './tailwind-styles/globals.css?inline';
-
-import './tailwind-styles/preflight.css';
-import preflight from './tailwind-styles/preflight.css?inline';
+import './tailwind-styles/global.css';
+import styles from './tailwind-styles/component.css?inline';
 
 export class BaseElement extends LitElement {
-  public static styles = [
-    unsafeCSS(preflight),
-    unsafeCSS(components),
-    unsafeCSS(utilities),
-    unsafeCSS(screens),
-    unsafeCSS(globals),
-  ].filter((styles) => styles.cssText !== '');
+  public static styles = [unsafeCSS(styles)];
 }
