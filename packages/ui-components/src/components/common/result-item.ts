8fa45b155b6e89989969690e9ce0bef4b45feff7
Jort Willemsen <jort@maurice-nienke.nl>
2023-10-19 16:26:33 +0200

    fix: newlines for result-item

--- packages/ui-components/src/components/common/result-item.ts
+++ packages/ui-components/src/components/common/result-item.ts
@@ -6,8 +6,10 @@ import { property } from 'lit/decorators.js';
 export class ResultItem extends BaseElement {
   @property({ type: String })
   resultName = '';
+
   @property({ type: Number })
   currentStep = 907;
+  
   @property({ type: Number })
   totalSteps = 1000;
 
