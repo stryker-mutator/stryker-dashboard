2a0b1ebac36e1983b50b757cd2fb3e4a750db5e8
Jort Willemsen <jort@maurice-nienke.nl>
2023-10-19 16:20:45 +0200

    fix: change function to ternary to find logo

--- packages/ui-components/src/components/top-bar.ts
+++ packages/ui-components/src/components/top-bar.ts
@@ -8,7 +8,7 @@ export class TopBar extends BaseElement {
 
   getLogo() {
     if (this.logo) {
-      return html`<img class="p-2 h-12" src="${this.logo}" />`;
+      return html``;
     } else {
       return html``;
     }
@@ -18,9 +18,14 @@ export class TopBar extends BaseElement {
     return html` <div
       class="text-white items-center bg-neutral-800 h-16 p-2 text-l flex flex-row"
     >
-      ${this.getLogo()}
+      ${this.logo && html`<img class="p-2 h-12" src="${this.logo}" />`}
       <h2 class="px-1">
-        <a href="/" class="hover:cursor-pointer hover:text-red-500 transition-all text-white no-underline font-bold">Stryker Mutator</a> | Dashboard
+        <a
+          href="/"
+          class="hover:cursor-pointer hover:text-red-500 transition-all text-white no-underline font-bold"
+          >Stryker Mutator</a
+        >
+        | Dashboard
       </h2>
       <div class="ml-auto"><slot name="user"></slot></div>
     </div>`;
