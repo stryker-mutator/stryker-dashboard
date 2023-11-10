d2ac986c8d2eefc4156af4d6642dbf63d836435d
Xander Vedder <xander.vedd@gmail.com>
2023-11-10 15:42:09 +0100

    refactor: use lit directives in profile-button

--- packages/stryker-elements/src/components/profile-button.ts
+++ packages/stryker-elements/src/components/profile-button.ts
@@ -1,6 +1,7 @@
 import { html } from 'lit';
 import { BaseElement } from '../base';
 import { property, state } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
 
 export class ProfileButton extends BaseElement {
   @property()
@@ -15,11 +16,11 @@ export class ProfileButton extends BaseElement {
   @state()
   clicked = false;
 
-  _handleClick = () => {
+  #handleClick = () => {
     this.clicked = !this.clicked;
   };
 
-  _navigateToRepositories = () => {
+  #navigateToRepositories = () => {
     window.location.href = `/repos/${this.name}`;
   };
 
@@ -28,30 +29,32 @@ export class ProfileButton extends BaseElement {
   }
 
   render() {
+    const directionClasses = classMap({
+      'text-right hover:border-l-8 pr-5': this.direction === 'right',
+      'text-left hover:border-r-8 pl-5': this.direction !== 'right',
+    });
+
     return html`<button
-        @click="${this._handleClick}"
+        @click="${this.#handleClick}"
         class="mr-4 h-10 w-10 overflow-hidden rounded-full border border border-2 border-solid  border-neutral-600"
       >
         <img class="h-full w-full" src="${this.avatarUrl}" />
       </button>
       <div
-        class="${this.clicked ? '' : 'hidden'} ${this.direction == 'right'
-          ? '-translate-x-40'
-          : ''} align-center absolute z-[999] flex w-48 translate-y-4 flex-col overflow-hidden rounded-md border border-2 border-neutral-600 bg-neutral-800"
+        class="${classMap({
+          hidden: !this.clicked,
+          '-translate-x-40': this.direction === 'right',
+        })} align-center absolute z-[999] flex w-48 translate-y-4 flex-col overflow-hidden rounded-md border border-2 border-neutral-600 bg-neutral-800"
       >
         <button
-          @click="${this._navigateToRepositories}"
-          class="${this.direction == 'right'
-            ? 'text-right hover:border-l-8 pr-5'
-            : 'text-left hover:border-r-8 pl-5'} h-10 border-b-2 border-neutral-600 text-white transition-all"
+          @click="${this.#navigateToRepositories}"
+          class="${directionClasses} h-10 border-b-2 border-neutral-600 text-white transition-all"
         >
           My repositories
         </button>
         <button
           @click="${this.#dispatchSignOut}"
-          class="${this.direction == 'right'
-            ? 'text-right hover:border-l-8 pr-5'
-            : 'text-left hover:border-r-8 pl-5'} h-10 border-neutral-600 text-white transition-all"
+          class="${directionClasses} border-b2 h-10 border-neutral-600 text-white transition-all"
         >
           Sign out
         </button>
