232b6add2d39ccbd1003421750f67e0d507e3383
Xander Vedder <xander.vedd@gmail.com>
2023-11-10 15:02:40 +0100

    chore: use actual private instead of underscore

--- packages/stryker-elements/src/components/sign-in-button.ts
+++ packages/stryker-elements/src/components/sign-in-button.ts
@@ -6,13 +6,13 @@ export class SignInButton extends BaseElement {
   @property({ type: String })
   url = '/api/auth/github';
 
-  _handleClick = () => {
+  #handleClick = () => {
     window.location.href = this.url;
   };
 
   render() {
     return html`<button
-      @click="${this._handleClick}"
+      @click="${this.#handleClick}"
       class="box-border h-10 rounded-lg border-2 border-solid border-white bg-neutral-800 p-2 pl-2 pr-2"
     >
       <p class="text-l font-bold text-white">Sign in with Github</p>
