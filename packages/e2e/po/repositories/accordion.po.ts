87ef54548431d96500d843456281db4255deed47
Xander Vedder <xander.vedd@gmail.com>
2023-12-22 13:53:14 +0100

    fix: e2e tests

--- packages/e2e/po/repositories/accordion.po.ts
+++ packages/e2e/po/repositories/accordion.po.ts
@@ -4,7 +4,7 @@ import { AccordionCardPageObject } from '../repositories/accordion-card.po.js';
 export class AccordionPageObject extends PageObject {
   public getCard(cardHeader: string): AccordionCardPageObject {
     const cardHost = this.host.locator(
-      `:has(.accordion-header:has-text("${cardHeader}"))`
+      `:has(.accordion-header button:has-text("${cardHeader}"))`
     );
     return new AccordionCardPageObject(cardHost);
   }
