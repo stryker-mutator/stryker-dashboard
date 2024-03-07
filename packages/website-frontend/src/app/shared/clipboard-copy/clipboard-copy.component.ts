7c751941510e2775352cb45fae20ff0aa62b2f33
Hugo van Rijswijk <git@hugovr.nl>
2024-03-07 12:39:50 +0100

    pin dependencies

--- packages/website-frontend/src/app/shared/clipboard-copy/clipboard-copy.component.ts
+++ packages/website-frontend/src/app/shared/clipboard-copy/clipboard-copy.component.ts
@@ -1,23 +1,23 @@
-import { Component, Input, ElementRef, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import '@github/clipboard-copy-element';
-import { copy } from '@primer/octicons';
+import { copy as copySvg } from '@primer/octicons';
 
 @Component({
   selector: 'stryker-clipboard-copy',
-  template: `<clipboard-copy [attr.for]="for"> </clipboard-copy>`,
+  template: `<clipboard-copy
+    [attr.for]="for"
+    [innerHtml]="copy"
+  ></clipboard-copy>`,
   host: {
     class: 'btn btn-sm pt-0',
   },
 })
-export class ClipboardCopyComponent implements OnInit {
+export class ClipboardCopyComponent {
   @Input()
   public for: string | undefined;
-
-  constructor(private readonly elementRef: ElementRef) {}
-
-  public ngOnInit(): void {
-    const el: HTMLElement = this.elementRef.nativeElement;
-    const svg = copy.toSVG();
-    el.querySelector('clipboard-copy')!.innerHTML = svg;
+  public copy: SafeHtml;
+  constructor(sanitizer: DomSanitizer) {
+    this.copy = sanitizer.bypassSecurityTrustHtml(copySvg.toSVG());
   }
 }
