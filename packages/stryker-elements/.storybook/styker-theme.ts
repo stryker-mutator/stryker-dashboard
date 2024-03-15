934637bd752b7d49747d86645205efecbaf92bc9
Lex Krooswijk <lexkrooswijk@live.nl>
2024-03-15 11:58:48 +0100

    add stryker logo and name

---
+++ packages/stryker-elements/.storybook/styker-theme.ts
@@ -0,0 +1,15 @@
+import { create } from '@storybook/theming/create'
+import { html } from 'lit'
+
+export default create({
+  base: 'dark',
+  brandTitle: `
+    <div style="display:grid;grid-template-columns:25px 1fr;grid-gap:5px;align-items:center;">
+      <img src="https://stryker-mutator.io/images/stryker.svg" width="25px" /> 
+      <span>Stryker</span>
+    </div>
+  `,
+  brandUrl: 'https://github.com/stryker-mutator/stryker-dashboard',
+  brandTarget: '_blank',
+})
+
