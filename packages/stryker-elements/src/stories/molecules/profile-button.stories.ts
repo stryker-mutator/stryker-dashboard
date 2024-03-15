627b947799f69d3ee0e532bf11f2cdd8bf9ade0d
Lex Krooswijk <lexkrooswijk@live.nl>
2024-03-15 14:02:46 +0100

    add profile button stories

---
+++ packages/stryker-elements/src/stories/molecules/profile-button.stories.ts
@@ -0,0 +1,30 @@
+import { Meta, StoryObj } from '@storybook/web-components'
+import { html } from 'lit'
+
+import '../../exports/lib/molecules/profile-button'
+
+export default {
+  title: 'Molecules/Profile Button',
+  component: 'sme-profile-button',
+} as Meta
+
+export const Default: StoryObj = {
+  name: 'Right Alignment',
+  render: () => {
+    return html`
+      <div style="display: flex; justify-content: flex-end;">
+        <sme-profile-button
+          avatarUrl="https://stryker-mutator.io/images/stryker.svg"
+        ></sme-profile-button>
+      </div>
+    `
+  },
+}
+
+export const LeftAlignment: StoryObj = {
+  name: 'Left Alignment',
+  args: {
+    avatarUrl: 'https://stryker-mutator.io/images/stryker.svg',
+    direction: 'left',
+  }
+}
