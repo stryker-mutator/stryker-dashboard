c89a273b2b55d749da0d2f81ba13ee0b5fa482af
Lex Krooswijk <lexkrooswijk@live.nl>
2024-03-15 12:09:58 +0100

    add hero story and remove unnecessary class

---
+++ packages/stryker-elements/src/stories/organisms/hero.stories.ts
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/web-components'
+
+import '../../exports/lib/organisms/hero'
+
+export default {
+  title: 'Organisms/Hero',
+  component: 'sme-hero',
+} as Meta
+
+export const Default: StoryObj = { }
