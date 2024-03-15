5c557d2d80cf481bfd775d7e1996b07da03721c0
Lex Krooswijk <lexkrooswijk@live.nl>
2024-03-15 13:38:31 +0100

    add the other organism stories

---
+++ packages/stryker-elements/src/stories/organisms/status-overview.stories.ts
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/web-components'
+
+import '../../exports/lib/organisms/status-overview'
+
+export default {
+  title: 'Organisms/Status Overview',
+  component: 'sme-status-overview',
+} as Meta
+
+export const Default: StoryObj = { }
