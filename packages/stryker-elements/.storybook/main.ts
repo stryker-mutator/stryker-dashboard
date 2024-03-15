9a4986ee86c05318e9a03eca7b76b1f63575d760
Lex Krooswijk <lexkrooswijk@live.nl>
2024-03-15 15:07:18 +0100

    remove unused addon in .storybook/main.ts

--- packages/stryker-elements/.storybook/main.ts
+++ packages/stryker-elements/.storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
   ],
   framework: {
     name: "@storybook/web-components-vite",
