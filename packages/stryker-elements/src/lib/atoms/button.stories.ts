85555b8b4006ca321db2b5213bbc09c57460562b
Lex Krooswijk <lexkrooswijk@live.nl>
2024-03-15 09:22:40 +0100

    add storybook and basic story

---
+++ packages/stryker-elements/src/lib/atoms/button.stories.ts
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/web-components'
+
+import './../../exports/lib/atoms/button'
+
+export default {
+  title: 'Button Element',
+  render: () => '<sme-button>Button</sme-button>',
+} as Meta
+
+export const Default: StoryObj = {
+}
