ac25d538ff4246a94ee70552a23aa8fd6d539b04
Lex <lexkrooswijk@live.nl>
2024-03-15 15:24:03 +0100

    feat(stryker-elements): Add Storybook (#548)
    add storybook

---
+++ packages/stryker-elements/src/stories/molecules/repository.stories.ts
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/web-components';
+
+import '../../exports/lib/molecules/repository';
+
+export default {
+  title: 'Molecules/Repository',
+  component: 'sme-repository',
+} as Meta;
+
+export const Default: StoryObj = {
+  args: {
+    name: 'stryker-mutator/stryker',
+    currentStep: 907,
+    totalSteps: 1000,
+  },
+};
---
+++ packages/stryker-elements/src/stories/organisms/stryker-dashboard-explanation.stories.ts
@@ -0,0 +1,10 @@
+import { Meta, StoryObj } from '@storybook/web-components';
+
+import '../../exports/lib/organisms/stryker-dashboard-explanation';
+
+export default {
+  title: 'Organisms/Stryker Dashboard Explanation',
+  component: 'sme-stryker-dashboard-explanation',
+} as Meta;
+
+export const Default: StoryObj = {};
