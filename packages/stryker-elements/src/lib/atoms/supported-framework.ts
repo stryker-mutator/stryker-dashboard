526e0c4ec3c03beee48926ac5247d8b97a5df8c1
Lex Krooswijk <lexkrooswijk@live.nl>
2024-03-15 16:48:50 +0100

    eod add supported frameworks atom start

---
+++ packages/stryker-elements/src/lib/atoms/supported-framework.ts
@@ -0,0 +1,31 @@
+import { html } from 'lit';
+import { BaseElement } from '../base-element.js';
+import { property } from 'lit/decorators.js';
+
+export class SupportedFramework extends BaseElement {
+  @property({ type: String })
+  name: string = '';
+
+  @property({ type: String })
+  logo: string = '';
+
+  @property({ type: String })
+  url: string = '';
+
+  render() {
+    return html`
+      <a
+        href="${this.url}"
+        target="_blank"
+        class="grid w-20 grid-cols-1 gap-1 grayscale"
+      >
+        <img
+          class="h-16 place-self-center"
+          src="${this.logo}"
+          alt="${this.name} logo"
+        />
+        <span class="text-center font-bold text-white">${this.name}</span>
+      </a>
+    `;
+  }
+}
