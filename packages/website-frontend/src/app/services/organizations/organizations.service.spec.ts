96db6d26c73192de64846eed05fd6f49dd9b5e05
Xander Vedder <xander.vedd@gmail.com>
2024-05-24 09:42:33 +0200

    fix: tests and button style

--- packages/website-frontend/src/app/services/organizations/organizations.service.spec.ts
+++ packages/website-frontend/src/app/services/organizations/organizations.service.spec.ts
@@ -1,15 +1,17 @@
-// import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
-// import { OrganizationsService } from './organizations.service';
+import { OrganizationsService } from './organizations.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-// describe('OrganizationsService', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [OrganizationsService]
-//     });
-//   });
+describe('OrganizationsService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [OrganizationsService]
+    });
+  });
 
-//   it('should be created', inject([OrganizationsService], (service: OrganizationsService) => {
-//     expect(service).toBeTruthy();
-//   }));
-// });
+  it('should be created', inject([OrganizationsService], (service: OrganizationsService) => {
+    expect(service).toBeTruthy();
+  }));
+});
