49fef5be9a1b21b5822c419ca648a04b4b35c01c
Xander Vedder <xander.vedd@gmail.com>
2024-09-07 16:29:43 +0200

    fix: name of test

--- packages/website-frontend/test/unit/services/history.service.spec.ts
+++ packages/website-frontend/test/unit/services/history.service.spec.ts
@@ -1,7 +1,7 @@
 import { historyService, HistoryService } from '../../../src/services/history.service';
 
 describe(HistoryService.name, () => {
-  it('should return an instance of Location', () => {
+  it('should return an instance of History', () => {
     expect(historyService.getHistory()).to.be.instanceOf(History);
   });
 });
