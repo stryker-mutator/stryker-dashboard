41fc5b32a60b2ee3b0250fd6eb87529cde72af04
Hugo van Rijswijk <git@hugovr.nl>
2026-03-16 11:54:05 +0100

    chore: update Vite library config

--- packages/stryker-elements/vite.config.ts
+++ packages/stryker-elements/vite.config.ts
@@ -1,21 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-const SOURCE_DIR = resolve(__dirname, 'src');
-const BUILD_DIR = resolve(__dirname, 'dist');
-const TYPES_DIR = resolve(__dirname, 'dist');
-// const TYPES_DIR = resolve(__dirname, 'types'); /* This is kind of better, but the node resolution angular project does not support it */
-// const TYPES_DIR = BUILD_DIR;
-
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    dts({
-      outDir: TYPES_DIR,
-    }),
-  ],
+  plugins: [tailwindcss(), dts()],
   css: {
     transformer: 'lightningcss',
   },
@@ -23,20 +11,18 @@ export default defineConfig({
     target: 'esnext',
     sourcemap: true,
     minify: false,
-    outDir: BUILD_DIR,
+    outDir: 'dist',
     emptyOutDir: true,
     lib: {
-      entry: resolve(SOURCE_DIR, 'main.ts'),
+      entry: 'src/main.ts',
     },
-    rollupOptions: {
-      external: /^lit/,
+    rolldownOptions: {
+      external: [/^lit/, /^@stryker-mutator/],
       output: [
         {
           format: 'es',
           preserveModules: true,
-          preserveModulesRoot: SOURCE_DIR,
           entryFileNames: '[name].js',
-          inlineDynamicImports: false,
         },
       ],
     },
